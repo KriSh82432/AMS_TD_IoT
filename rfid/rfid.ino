9bdcfac9762fdb43dc25aa9928608aaf3199392e
Krishna V <kg5300@srmist.edu.in>
2022-09-14 22:11:20 +0530

    Commit#2

---
+++ rfid/rfid.ino
@@ -0,0 +1,45 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <SoftwareSerial.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);// Create MFRC522 instance.
+SoftwareSerial esp(5,6);
+ 
+void setup() 
+{
+  Serial.begin(115200);   // Initiate a serial communication
+  esp.begin(115200);
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  //Serial.println("Scan your card now...");
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  //Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  //Serial.println();
+  //Serial.println("Please Wait...");
+  delay(3000);
+  //Serial.println("Scan your card now!!");
+}
