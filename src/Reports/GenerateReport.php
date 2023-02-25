9e8586aaa7fdba4705a03a7e8ba9cc00f646e1a2
Krishna V <kg5300@srmist.edu.in>
2023-02-25 23:14:29 +0530

    Update GenerateReport.php

--- src/Reports/GenerateReport.php
+++ src/Reports/GenerateReport.php
@@ -6,8 +6,8 @@ require 'GoogleSheets/Reports.php';
 
 // Format : <Time> <Date> <Month> <Year>
 // Example : 6.30 PM 4 February 2023
-$startDate = '11 AM 25 February 2023';
-$endDate = '5 PM 25 February 2023';
-$tabName = 'Feb 25, 2023 - AFTERNOON';
+$startDate = ''; //Start time
+$endDate = ''; //End Time
+$tabName = ''; //Tab Name in a spreadsheet
 
 $bot = new \GoogleSheets\Reports\Report($startDate, $endDate, $tabName);
